2c412e8ae95e2721af2fe1a1f0d27c48ba802e5e
Owen Pickford <owen.pickford@snyk.io>
2025-08-13 10:00:08 -0700

    Make the port configurable

--- app.py
+++ app.py
@@ -89,4 +89,5 @@ def index():
 
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=5000, debug=False)
+	port = int(os.environ.get("PORT", 8081))
+	app.run(host="0.0.0.0", port=port, debug=False)
